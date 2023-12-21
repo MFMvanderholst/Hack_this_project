566dbbf68d6314da8b59057193c6872d9b208840
MFMvanderholst <117632371+MFMvanderholst@users.noreply.github.com>
2023-12-21 15:57:25 +0100

    fixed bugs

--- js/api.js
+++ js/api.js
@@ -17,38 +17,3 @@ fetchData('#c');
 fetchData('#d');
 fetchData('#e');
 fetchData('#f');
-
-
-// url variable
-const gameUrl = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15';
-textTitle = "";
-
-async function fetchData() {
-    try {
-        const response = await fetch(gameUrl);
-        const result = await response.json();
-        result.forEach(myFunction);
-
-        function myFunction(item) {
-            
-            const title = item.title;
-            const dealRating = item.dealRating;
-            const normalPrice = item.normalPrice;
-            const salePrice = item.salePrice;
-
-            textTitle += `title: ${title}<br>
-                        rating: ${dealRating}<br>
-                        originalPrice: ${normalPrice}<br>
-                        salesPrice: ${salePrice}<br>
-                                                <br>`;
-        }
-
-        document.querySelector('.title').innerHTML = textTitle;
-        
-        console.log(result);
-    } catch (error) {
-        console.error(error);
-    }
-}
-
-fetchData();
