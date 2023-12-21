b0b5ab361812851c49331b6d090f0cdb6d4a26cd
MFMvanderholst <117632371+MFMvanderholst@users.noreply.github.com>
2023-12-21 15:50:25 +0100

    finally found a game api

--- js/api.js
+++ js/api.js
@@ -1,17 +1,56 @@
-const url = 'https://dog.ceo/api/breeds/image/random';
-messeges: [];
-
-
-async function fetchData() {
+const dogUrl = 'https://dog.ceo/api/breeds/image/random';
+async function fetchData($x) {
     try {
-        const response = await fetch(url);
+        const response = await fetch(dogUrl);
         const result = await response.json();
-        document.getElementById("myImg").src = result.message;
-        console.log(result);
+        document.getElementById($x).src = result.message;
+        // console.log(result);
     } catch (error) {
         console.error(error);
     }
 }
 
 // Call the asynchronous function
+fetchData('#a');
+fetchData('#b');
+fetchData('#c');
+fetchData('#d');
+fetchData('#e');
+fetchData('#f');
+
+
+
+
+// url variable
+const gameUrl = 'https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15';
+textTitle = "";
+
+async function fetchData() {
+    try {
+        const response = await fetch(gameUrl);
+        const result = await response.json();
+        result.forEach(myFunction);
+
+        function myFunction(item) {
+            
+            const title = item.title;
+            const dealRating = item.dealRating;
+            const normalPrice = item.normalPrice;
+            const salePrice = item.salePrice;
+
+            textTitle += `title: ${title}<br>
+                        rating: ${dealRating}<br>
+                        originalPrice: ${normalPrice}<br>
+                        salesPrice: ${salePrice}<br>
+                                                <br>`;
+        }
+
+        document.querySelector('.title').innerHTML = textTitle;
+        
+        console.log(result);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
 fetchData();
